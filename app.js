9c24c6ec476f8667b46f461dd9fd8477838d4944
Retroxs <retroxsx@gmail.com>
2018-03-08 22:44:52 +0800

    fix: utc +8

--- app.js
+++ app.js
@@ -5,20 +5,21 @@ const time_range = utils.time_range
 // Create a bot that uses 'polling' to fetch new updates
 const token = process.env.avdog_token;
 const bot = new TelegramBot(token, { polling: true });
-const NSFW = (time_range(9,11) || time_range(14,23));
 
 async function handleAv(msg, match) {
-  const chatId = msg.chat.id;
+  let chatId = msg.chat.id;
+  let NSFW = (time_range(9, 11) || time_range(14, 18));
   if (NSFW) {
     let dailyverse = await r2('http://dailyverse-qoli.appspot.com').text
-    bot.sendMessage(chatId, dailyverse)
+    bot.sendMessage(chatId, `工作时间(上午:9-11,下午:14-18)禁止开车:\n${dailyverse.trim()}`)
   } else {
-    let av = await r2(`https://api.avgle.com/v1/video/${match[1]}`).html
+    let av = await r2(`https://api.avgle.com/v1/video/${match[1]}`).json
+    console.log(av)
     if (av.success) {
       let video = av.response.video
       bot.sendMessage(chatId, video.video_url)
     } else {
-      bot.sendMessage(chatId, "未找到相应资源")
+      bot.sendMessage(chatId, "未找到相应资源,请尝试新车牌")
     }
   }
 }
--- utils.js
+++ utils.js
@@ -4,9 +4,10 @@ const moment = MomentRange.extendMoment(Moment);
 
 module.exports = {
     time_range(startTime, endTime) {
-        let start = moment().hours(startTime).minutes(0).second(0)
-        let end = moment().hours(endTime).minutes(0).second(0)
-        let range = moment.range(start, end);
-        return range.contains(moment().utcOffset(8))
+        let start = moment.utc().utcOffset(8).hours(startTime).minutes(0).second(0)
+        let end = moment.utc().utcOffset(8).hours(endTime).minutes(0).second(0)
+        let range = moment.utc().utcOffset(8).range(start, end);
+        return range.contains(new Date())
     }
 }
+
